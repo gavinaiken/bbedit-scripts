df4cc30e82e35c09253a13e1aee791c261a7ca9f
Gavin Aiken <gavin@aiken.org.uk>
2015-01-12 20:03:08 +0000

    Support strikethrough, auto overflow on code blocks

--- Contents/Preview Filters/DefaultFilter_Markdown.rb
+++ Contents/Preview Filters/DefaultFilter_Markdown.rb
@@ -7,8 +7,10 @@ r = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
   :autolink => true,
   :space_after_headers => true,
   :fenced_code_blocks => true,
+  :disable_indented_code_blocks => true,
   :no_intra_emphasis => true,
   :lax_html_blocks => true,
+  :strikethrough =>true,
   :tables => true)
 
 input = ARGF.read
