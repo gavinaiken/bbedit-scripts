63698cbf4e62dd1cc488e157f295ca8138ef5a2e
Gavin Aiken <gavin.aiken@nuviapp.com>
2015-07-29 11:28:29 -0600

    Fix symlink script for case where css and filter folders do not yet exist

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -11,11 +11,17 @@ filter_filename="DefaultFilter_Markdown.rb"
 my_dir=$( dirname "$0" )
 cd "$my_dir"
 
-if [ ! -d "../../$css_dirname" ] || [ ! -d "../../$filter_dirname" ] || [ ! -d "../$package_dirname" ]
+home_dir=~
+expected_dir="$home_dir/Library/Application Support/BBEdit/Packages/$package_dirname"
+if [ "`pwd`" != "$expected_dir" ]
 then
-    echo "Package does not seem to be in the correct directory, it should be in ~/Library/Application Support/BBEdit/Packages/$package_dirname"
+    echo "Package does not seem to be in the correct directory, it should be in $expected_dir"
     exit 1
 fi
+
+[ ! -d "../../$css_dirname" ] && mkdir "../../$css_dirname"
+
+[ ! -d "../../$filter_dirname" ] && mkdir "../../$filter_dirname"
 
 ln -s "../Packages/$package_dirname/Contents/$css_dirname/$css_filename" "../../$css_dirname"
 ln -s "../Packages/$package_dirname/Contents/$filter_dirname/$filter_filename" "../../$filter_dirname"
