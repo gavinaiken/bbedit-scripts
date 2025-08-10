a5aa13213a1f9a3664fbccee741b2b67cd88dae6
Christopher Waterman <chris@rustydogink.com>
2025-08-10 16:04:13 -0700

    Added optional syntax highlighting for code blocks. The rouge gem is loaded and included if available, otherwise it functions as before.

--- Contents/Preview Filters/DefaultFilter_Markdown.rb
+++ Contents/Preview Filters/DefaultFilter_Markdown.rb
@@ -1,24 +1,39 @@
 #!/usr/bin/env ruby
 
 require 'redcarpet'
-require 'rouge'
-require 'rouge/plugins/redcarpet'
 
-class MarkdownHighlightRender < Redcarpet::Render::HTML
- def initialize(extensions = {})
-	 super extensions.merge(link_attributes: { target: '_blank' })
- end
- include Rouge::Plugins::Redcarpet
+module Compatability
+	private
+	
+	def hightlighting?
+		require 'rouge'
+		require 'rouge/plugins/redcarpet'
+	rescue LoadError
+		false
+	end
 end
 
-r = Redcarpet::Markdown.new(MarkdownHighlightRender, 
-  :autolink => true,
-  :space_after_headers => true,
-  :fenced_code_blocks => true,
-  :disable_indented_code_blocks => true,
-  :no_intra_emphasis => true,
-  :lax_html_blocks => true,
-  :strikethrough =>true,
-  :tables => true)
+class SyntaxHighlightRenderer < Redcarpet::Render::HTML	
+	extend Compatability
+	include Rouge::Plugins::Redcarpet if hightlighting?
+	
+	def initialize(extensions = {})
+		super extensions.merge(link_attributes: { target: '_blank' })
+	end
+end
 
-input = ARGF.read
+md_to_html = Redcarpet::Markdown.new(
+		SyntaxHighlightRenderer, 
+		:autolink => true,
+		:space_after_headers => true,
+		:fenced_code_blocks => true,
+		:disable_indented_code_blocks => true,
+		:no_intra_emphasis => true,
+		:lax_html_blocks => true,
+		:strikethrough =>true,
+		:tables => true
+  )
+
+input_markdown = ARGF.read
+
+puts md_to_html.render(input_markdown)
