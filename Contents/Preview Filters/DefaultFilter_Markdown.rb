6f3ca481edfd7716540729052d23ea5ca93ab685
Christopher Waterman <chris@rustydogink.com>
2025-08-13 15:24:33 -0700

    Standardized indention around 4 spaces.

--- Contents/Preview Filters/DefaultFilter_Markdown.rb
+++ Contents/Preview Filters/DefaultFilter_Markdown.rb
@@ -3,35 +3,35 @@
 require 'redcarpet'
 
 module Compatability
-	private
-	
-	def hightlighting?
-		require 'rouge'
-		require 'rouge/plugins/redcarpet'
-	rescue LoadError
-		false
-	end
+    private
+    
+    def hightlighting?
+        require 'rouge'
+        require 'rouge/plugins/redcarpet'
+    rescue LoadError
+        false
+    end
 end
 
 class SyntaxHighlightRenderer < Redcarpet::Render::HTML	
-	extend Compatability
-	include Rouge::Plugins::Redcarpet if hightlighting?
-	
-	def initialize(extensions = {})
-		super extensions.merge(link_attributes: { target: '_blank' })
-	end
+    extend Compatability
+    include Rouge::Plugins::Redcarpet if hightlighting?
+    
+    def initialize(extensions = {})
+        super extensions.merge(link_attributes: { target: '_blank' })
+    end
 end
 
 md_to_html = Redcarpet::Markdown.new(
-		SyntaxHighlightRenderer, 
-		:autolink => true,
-		:space_after_headers => true,
-		:fenced_code_blocks => true,
-		:disable_indented_code_blocks => true,
-		:no_intra_emphasis => true,
-		:lax_html_blocks => true,
-		:strikethrough =>true,
-		:tables => true
+    SyntaxHighlightRenderer, 
+    :autolink => true,
+    :space_after_headers => true,
+    :fenced_code_blocks => true,
+    :disable_indented_code_blocks => true,
+    :no_intra_emphasis => true,
+    :lax_html_blocks => true,
+    :strikethrough =>true,
+    :tables => true
   )
 
 input_markdown = ARGF.read
